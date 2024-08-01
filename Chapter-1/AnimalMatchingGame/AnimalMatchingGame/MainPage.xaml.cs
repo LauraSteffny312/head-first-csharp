6e0759d2ecc9bbf7e5492f69a7a7c38f4cd7cd28
LauraSteffny312 <la.steffny@live.de>
2024-08-01 17:53:23 +0200

    feature: initialize .NET MAUI, implement first layout draft

---
+++ Chapter-1/AnimalMatchingGame/AnimalMatchingGame/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace AnimalMatchingGame;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void PlayAgainButton_Clicked(object sender, EventArgs e)
+	{
+
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		
+	}
+}
+
