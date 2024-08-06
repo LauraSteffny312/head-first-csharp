99ca35983eb27002f8fba37a857e3cb42ee6d0f7
LauraSteffny312 <la.steffny@live.de>
2024-08-06 21:55:46 +0200

    feature: implement Chapter-3 Pick Random Cards MAUI App

---
+++ Chapter-3/PickRandomCardsMAUI/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using PickRandomCards;
+
+namespace PickRandomCardsMAUI;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void PickCardsButton_clicked(object sender, EventArgs e)
+	{
+        if (int.TryParse(NumberOfCards.Text, out int numberOfCards))
+        {
+			PickedCards.Text = string.Empty;
+            string[] cards = CardPicker.PickSomeCards(numberOfCards);
+            foreach (string card in cards)
+            {
+                PickedCards.Text += card + Environment.NewLine;
+            }
+			PickedCards.Text += Environment.NewLine + "You picked " + numberOfCards + " cards."; 
+        }
+        else
+        {
+            PickedCards.Text = "Please enter a valid number.";
+        }
+	}
+}
+
